addeabac1c498f092b0cebe93967745a528c97ff
rider <ridebus82@nate.com>
2024-08-21 15:42:52 +0900

    240821 asdlfjgogogogo

--- ezip_front/front/src/routes/(app)/view/[id]/+page.svelte
+++ ezip_front/front/src/routes/(app)/view/[id]/+page.svelte
@@ -43,6 +43,8 @@
 
     onMount(() => {
         try {
+            console.log(import.meta.env.VITE_KAKAO_JSKEY);
+            
             Kakao.init(import.meta.env.VITE_KAKAO_JSKEY);
             console.log(Kakao);
 
